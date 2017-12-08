c034fc1c16da565eb56ed1545c0f4a6e6a88c431
Joey Feng <jfeng702@gmail.com>
2017-12-07 19:37:03 -0800

    Queue complete

---
+++ W1D5/stack.rb
@@ -0,0 +1,23 @@
+class Stack
+  # LIFO
+
+  def initialize
+    # create ivar to store stack here!
+    @stack = []
+  end
+
+  def add(el)
+    # adds an element to the stack
+    @stack << el
+  end
+
+  def remove
+    # removes one element from the stack
+    @stack.pop
+  end
+
+  def show
+    # return a copy of the stack
+    @stack
+  end
+end
