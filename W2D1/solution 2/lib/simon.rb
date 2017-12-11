97e6849d5e75ea9e0ebc9c2dde964922b6dafb19
Joey Feng <jfeng702@gmail.com>
2017-12-10 20:38:45 -0800

    Fixed timing

--- W2D1/solution 2/lib/simon.rb
+++ W2D1/solution 2/lib/simon.rb
@@ -10,19 +10,20 @@ class Simon
   end
 
   def play
-    until game_over
+    until @game_over
       take_turn
-      system('clear')
+      system("clear")
     end
 
     game_over_message
     reset_game
   end
 
-  def take_turn(*args)
-    unless game_over
-      show_sequence
-      require_sequence
+  def take_turn
+    show_sequence
+    require_sequence
+
+    unless @game_over
       round_success_message
       @sequence_length += 1
     end
@@ -30,48 +31,44 @@ class Simon
 
   def show_sequence
     add_random_color
-    seq.each do |color|
+    @seq.each do |color|
       puts color
       sleep 0.75
-      system('clear')
+      system("clear")
       sleep 0.25
     end
   end
 
   def require_sequence
-    puts "What is the sequence? red blue green yellow"
-
-    seq.each do |color|
-      guess = gets.chomp
-      if color[0] != guess
-        puts "you done messed up!"
+    puts "Repeat the sequence by entering the first letter of each color on a new line."
+    @seq.each do |color|
+      user_color = gets.chomp
+      if color[0] != user_color
         @game_over = true
         break
       end
     end
-    sleep 0.75
+    sleep 0.25
   end
 
   def add_random_color
-    self.seq << COLORS.sample
+    @seq << COLORS.shuffle[0]
   end
 
   def round_success_message
-    "you did good."
+    "Round successful. Here's the next sequence:"
   end
 
   def game_over_message
-    puts "Game over! You made it #{sequence_length-1} rounds"
+    puts "Game over! You made it #{@sequence_length - 1} rounds."
   end
 
   def reset_game
-    self.sequence_length = 1
-    self.game_over = false
-    self.seq = []
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
 
-if $PROGRAM_NAME == __FILE__
-  game = Simon.new
-  game.play
-end
+game = Simon.new
+game.play
