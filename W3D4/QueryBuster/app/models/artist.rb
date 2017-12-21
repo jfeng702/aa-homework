874efffcca18bf39dcc8e0daec5e7fff5477fefc
Joey Feng <jfeng702@gmail.com>
2017-12-21 00:01:37 -0800

    updated

---
+++ W3D4/QueryBuster/app/models/artist.rb
@@ -0,0 +1,44 @@
+class Artist < ApplicationRecord
+  has_many :albums,
+    class_name: 'Album',
+    foreign_key: :artist_id,
+    primary_key: :id
+
+  def n_plus_one_tracks
+    albums = self.albums
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.title] = album.tracks.length
+    end
+
+    tracks_count
+  end
+
+  def includes_tracks
+    albums = self.albums.includes(:tracks)
+    album_track_counts = {}
+    albums.each do |album|
+      album_track_counts[album] = album.tracks.length
+    end
+
+    album_track_counts
+  end
+
+  def better_tracks_query
+    albums_with_tracks = self
+      .albums
+      .select('albums.*, COUNT(*) AS track_count')
+      .joins(:tracks)
+      .group('albums.id')
+
+      # albums_with_tracks.map do |album|
+      #   [album.title, album.track_count]
+      # end
+
+      album_counts = {}
+      albums_with_tracks.each do |album|
+        album_counts[album.title] = album.track_count
+      end
+      album_counts
+  end
+end
